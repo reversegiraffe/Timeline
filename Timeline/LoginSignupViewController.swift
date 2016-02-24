6c9e97fdf4cd0a7c74554cca1d06c46f0e17d767
Kaytee Cannon <kaytee.cannon@mac.com>
2016-02-24 16:00:47 -0700

    Part 2 - Signin/Login View
    Class implementation through step 4

--- Timeline/LoginSignupViewController.swift
+++ Timeline/LoginSignupViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class LoginSignupViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var bioTextField: UITextField!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    var mode: ViewMode = .Signup
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +29,6 @@ class LoginSignupViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
@@ -32,4 +40,8 @@ class LoginSignupViewController: UIViewController {
     }
     */
 
+}
+
+enum ViewMode {
+    case Login, Signup
 }
