e35eb4ac1e0d0ee6a9a2be39e737a2cca88c3b99
Kaytee Cannon <kaytee.cannon@mac.com>
2016-02-26 16:42:27 -0700

    Fixed bugs, started Profile View
    Search now works (usernames lowercasestring),
    search tableview segue to profile view fixed.
    
    Added collection view and reusable collection view to profile view

--- Timeline/UserSearchTableViewController.swift
+++ Timeline/UserSearchTableViewController.swift
@@ -114,50 +114,16 @@ class UserSearchTableViewController: UITableViewController, UISearchResultsUpdat
     
     func updateSearchResultsForSearchController(searchController: UISearchController) {
         
-        let searchTerm = searchController.searchBar.text ?? ""
-        let lowercaseSearchTerm = searchTerm.lowercaseString
+        let searchTerm = searchController.searchBar.text?.lowercaseString ?? ""
         
         if let resultsController = searchController.searchResultsController as? UserSearchResultsTableViewController {
-            resultsController.usersResultsDataSource = usersDataSource.filter({$0.username.lowercaseString.containsString(lowercaseSearchTerm)})
             
+            resultsController.usersResultsDataSource = usersDataSource.filter({$0.username.lowercaseString.containsString(searchTerm)})
             resultsController.tableView.reloadData()
         }
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     
     // MARK: - Navigation
